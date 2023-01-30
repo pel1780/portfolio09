d8918f8d580106b72a566763f341f84601f593d1
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-30 17:47:16 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,4 +2,48 @@ $(function () {
     $('.topBanner .inner button').on('click', function () {
         $('.topBanner').addClass('on');
     })
+    // banner
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 100) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
+    // header
+
+    $('.mainSlide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.mainVisual .slide_dots li').on('click', function () {
+        var slideListNum = $(this).index();
+        $('.mainSlide').slick('slickGoTo', slideListNum);
+    });
+
+    $('.mainSlide').on('afterChange', function (e, s, c) {
+        $('.mainVisual .slide_dots li').eq(c).addClass('on').siblings().removeClass('on');
+    });
+    //main visual  main slide
+
+    $('.salon_left_slide ul').slick({
+        arrows: true,
+        fade: true,
+        asNavFor: '.salon_right_slide ul',
+    });
+
+    $('.salon_right_slide ul').slick({
+        arrows: false,
+        slidesToShow: 4,
+        asNavFor: '.salon_left_slide ul',
+        initialSlide: 1,
+    });
+
 });
