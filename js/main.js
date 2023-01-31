1b7b40def8098cdce2e76330230a961f199012e4
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-31 17:48:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(function () {
     $('.topBanner .inner button').on('click', function () {
         $('.topBanner').addClass('on');
-    })
+    });
     // banner
 
     $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
-        if (sct > 100) {
+        if (sct >= 100) {
             $('.header').addClass('on')
         } else {
             $('.header').removeClass('on')
         }
-    })
+    });
     // header
 
     $('.mainSlide').slick({
@@ -33,17 +33,42 @@ $(function () {
     });
     //main visual  main slide
 
-    $('.salon_left_slide ul').slick({
-        arrows: true,
+    $('.salon_left_slide').slick({
+        arrows: false,
         fade: true,
-        asNavFor: '.salon_right_slide ul',
+        asNavFor: '.salon_right_slide',
     });
 
-    $('.salon_right_slide ul').slick({
+    $('.salon_right_slide').slick({
         arrows: false,
         slidesToShow: 4,
-        asNavFor: '.salon_left_slide ul',
-        initialSlide: 1,
+        asNavFor: '.salon_left_slide',
     });
 
+    $('.mainSalon .salon_slide_case .slide_arrows .prev').on('click', function () {
+        $('.salon_left_slide').slick('slickPrev');
+    });
+    $('.mainSalon .salon_slide_case .slide_arrows .next').on('click', function () {
+        $('.salon_left_slide').slick('slickNext');
+    });
+
+    ///////////// main script //////////////
+
+    $('.coll_slide').slick({
+        arrows: false,
+        fade: true,
+    });
+
+    $('.coll_slide').on('wheel', function (e) {
+        if (e.originalEvent.deltaY < 0) {
+            $('.coll_slide').slick('slickPrev');
+        } else {
+            $('.coll_slide').slick('slickNext');
+        }
+    });
+
+    $('.coll_slide').on('init afterChange', function (e, s, c) {
+        $('.num strong').text(c ? ('0' + (c + 1)) : ('0' + 1));
+        $('.num span').text(('0' + s.slideCount));
+    })
 });
